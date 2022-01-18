6d340a8dd00915d235e32ccde8a3f6cfb8cfce06
geannx <santiago.geanne@gmail.com>
2022-01-18 23:53:41 +0800

    1/18/2022
    css style for sidebar

---
+++ student_loginDash/studentDash.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Dashboard</title>
+    <link rel="stylesheet" href="style_sDash.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <!--PUP LOGO-->
+            <div class="profile">
+                <img src="puplogo.png" alt="">
+                <!--Full Name ng Student-->
+                <h3 id="student_name"><br>Dela Cruz, Juan M.</h3>
+                <!--Student Number-->
+                <p id="student_id">2019-*****-MN-0</p>
+            </div>
+            <!--Sidebar Menu Items-->
+            <ul>
+                <li><a href="#">
+                    <span class="smenu_icon"><i class="far fa-folder-open"></i></span>
+                    <span class="smenu_item">View History</span></a></li>
+                <li><a href="#">
+                    <span class="smenu_icon"><i class="far fa-edit"></i></span>
+                    <span class="smenu_item">Write New Ticket</span></a></li>
+            </ul>
+            <div class="footer">  
+                <div>&copy Polytechnic University of the Philippines College of Computer Information and Sciences 2022
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
