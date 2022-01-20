cac4385beb16d451283f3abbdc635b9341fbbb9e
geannx <santiago.geanne@gmail.com>
2022-01-20 20:10:49 +0800

    hehe

--- student_loginDash/studentDash.php
+++ student_loginDash/studentDash.php
@@ -12,28 +12,43 @@
 <body>
     <div class="wrapper">
         <div class="sidebar">
-            <!--PUP LOGO-->
+                <!--PUP LOGO-->
             <div class="profile">
                 <img src="puplogo.png" alt="">
-                <!--Full Name ng Student-->
+                    <!--Full Name ng Student-->
                 <h3 id="student_name"><br>Dela Cruz, Juan M.</h3>
-                <!--Student Number-->
+                    <!--Student Number-->
                 <p id="student_id">2019-*****-MN-0</p>
             </div>
-            <!--Sidebar Menu Items-->
+                <!--Sidebar Menu Items-->
             <ul>
-                <li><a href="#">
+                <li>
+                    <a href="#" class="active">
+                    <span class="smenu_icon"><i class="fas fa-home"></i></span>
+                    <span class="smenu_item">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
                     <span class="smenu_icon"><i class="far fa-folder-open"></i></span>
-                    <span class="smenu_item">View History</span></a></li>
-                <li><a href="#">
+                    <span class="smenu_item">View History</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
                     <span class="smenu_icon"><i class="far fa-edit"></i></span>
-                    <span class="smenu_item">Write New Ticket</span></a></li>
+                    <span class="smenu_item">Write New Ticket</span>
+                    </a>
+                </li>
             </ul>
             <div class="footer">  
                 <div>&copy Polytechnic University of the Philippines College of Computer Information and Sciences 2022
                 </div>
             </div>
         </div>
+    </div>
+    <div class="Page Home">
+
     </div>
 </body>
 
