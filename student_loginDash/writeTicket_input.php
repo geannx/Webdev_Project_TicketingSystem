5b9243b3afd884557a8abe8b5ff0c98e54b1aa2d
geannx <santiago.geanne@gmail.com>
2022-02-12 18:56:58 +0800

    try lang hehe

--- student_loginDash/writeTicket_input.php
+++ student_loginDash/writeTicket_input.php
@@ -5,8 +5,9 @@
         $recipient = $_POST['faculty_name'];
         $message = $_POST['ta_message'];
     
-        $sql = "INSERT INTO tickets_main VALUES ('1', 'Gregorio', '2019-03460-MN-0', 'College of Science', '$recipient', 'On-going', CURDATE());";
-        $sql2 = "INSERT INTO ticket_ref VALUES ('1','$message','1')";
+        $sql = "INSERT INTO tickets_main VALUES ('8', 'Gory', '2019-03460-MN-0', 'College of Science', '$recipient', 'On-going', CURDATE());";
+        $sql2 = "INSERT INTO ticket_ref VALUES ('8','$message','1')
+                    ";
         
         mysqli_query($conn, $sql);
         mysqli_query($conn, $sql2);
