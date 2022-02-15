dd7895adc3f8c03acf7eca3817fd4a2e4ad648dc
Mark Steven Reyes <mstevenreyes@gmail.com>
2022-02-16 02:25:34 +0800

    Added reply interface with history

--- faculty-dashboard/reply_interface.php
+++ faculty-dashboard/reply_interface.php
@@ -15,25 +15,45 @@
         </div>
     </div>
     </div>
-    <div class="ticket-history">
+    <div class="ticket-history-bg">
+        <h2>Ticket Reply History</h2>
         <div class="ticket-history-inner">
+            <div class="ticket-history-object">
             <?php
-                echo "<table> " . "</table>";
+                $sql = "SELECT * FROM ticket_details WHERE ticket_number = ?";
+                $stmt = mysqli_stmt_init($conn);
+                mysqli_stmt_prepare($stmt, $sql);
+                mysqli_stmt_bind_param($stmt, "s", $_POST['ticketnum'] );
+                mysqli_stmt_execute($stmt);
+                $result1 = mysqli_stmt_get_result($stmt);
+                if(!$result1){
+                    echo "Empty Conversation.";
+                }else{
+                    while($result2 = mysqli_fetch_assoc($result1)){
+                        echo "<br><br>Sender: " . $result2['SenderID'] . "<br>Timestamp: " . $result2['timestamp'] . "<br><br>" . $result2['Body'];
+                    }
+                }
+
             ?>
+            
+            </div>        
         </div>
     </div>
     <div class="text_form">
-    <h1>Ticket Details:</h1><br>
-    <label>Ticket Number: <?php echo $row['ticket_number'];?></label><br>
-    <label>Student ID: <?php echo $row['StudentID']; ?></label><br>
-    <label>Subject: <?php echo $row['Subject']; ?></label>
-    <h1>Ticket Reply History: </h1>
+        <div class="ticket-details">
+            <h1>Ticket Details:</h1><br>
+            <label>Ticket Number: <?php echo $row['ticket_number'];?></label><br>
+            <label>Student ID: <?php echo $row['StudentID']; ?></label><br>
+            <label>Subject: <?php echo $row['Subject']; ?></label><br>
+        </div>
     
         <form action="reply_interface_inc.php" method="POST">
         <div class="text_box">
             <textarea class="text" name="MessageBody" placeholder="Your reply here..."></textarea>
         </div>
-            <input type="submit" class="button" value="Reply to Ticket">
+        <div class="reply-button-bg">
+            <input type="submit" class="reply-button" value="Reply to Ticket">
+        </div>
         </form>
     </div>
 </body>
