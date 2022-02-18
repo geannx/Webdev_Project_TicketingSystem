a3007f8734eabc2f33fd543671e13b2cf8f3595f
Mark Steven Reyes <mstevenreyes@gmail.com>
2022-02-18 20:20:01 +0800

    Added new class db connectivity, new ticket sent message

--- faculty_dashboard/new_ticket_interface.php
+++ faculty_dashboard/new_ticket_interface.php
@@ -9,8 +9,25 @@ include_once 'faculty_sidebar.php';
                 </div>
             </div>
     </div>
+    <div class="select">
+        <select name="faculty_name" id="faculty">
+        <option selected disabled>----------</option>
+        <?php
+            require '../dbh_inc.php';
+            $sql = "SELECT StudentID FROM class_list cd JOIN class_details cl ON cl.class_id = cd.class_id WHERE class_head = ?" ;
+            $stmt = mysqli_stmt_init($conn);
+                    mysqli_stmt_prepare($stmt, $sql);
+                    mysqli_stmt_bind_param($stmt, "s", $_SESSION['FacultyID'] );
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);
+            while($row = mysqli_fetch_array($result)){
+        ?> 
+            <option value="<?php echo $row['StudentID']; ?>"><?php echo $row['StudentID']; ?></option>
+            <?php } ?>
+        </select>          
+    </div>
     <div class="text_form">
-        <form action="#" method="POST">
+        <form action="ticket_sent.php" method="POST">
             
 
         <div class="text_box">
---
+++ faculty_dashboard/ticket_sent.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'faculty_sidebar.php';
+?>
+
+    <!-- Text box -->
+    <div class="section">
+                <div class="nav">
+                    <h1>Create New Ticket</h1>
+                </div>
+            </div>
+    </div>
+        <h1>Ticket sent.</h1>
+    </div>
+</body>
+
+</html>
