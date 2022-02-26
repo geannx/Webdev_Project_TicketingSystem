da50cb02c6bcb1d37255cc49920f6de4755be344
geannx <santiago.geanne@gmail.com>
2022-02-27 00:02:21 +0800

    UI changes ng slight

--- student_dashboard/history.php
+++ student_dashboard/history.php
@@ -1,15 +1,6 @@
 <?php 
     include 'student_sidebar.php';
 ?>
-
-<head>
-    <!-- Include Style Sheet -->
-    <style>
-        <?php include "style_sDash.css";?>
-    </style>
-</head>
-
-<body>
         <!-- GREETINGS HEADER -->
         <div class="section">
             <div class="greetings">
@@ -19,7 +10,7 @@
 
         <!-- Ticket History Container Box -->
         <div class="container">
-            <table class ="table-history">
+            <table class ="table-history" border = 2>
                 <tr>
                     <th class = "tb_columns">Ticket Number:</th>
                     <th class = "tb_columns">Recipient Faculty:</th>
@@ -32,22 +23,23 @@
                 <?php
                     include_once 'dbh_ticket.php';
                     //Getting Tickets according to student ID
-                    $sql = "SELECT * from ticket_details WHERE StudentID = ?";
+                    $sql = "SELECT *, FacultyName from ticket_details td JOIN faculty f on td.FacultyID = f.FacultyID WHERE td.StudentID = ?";
                     $stmt = mysqli_stmt_init($conn);
                     mysqli_stmt_prepare($stmt, $sql);
                     mysqli_stmt_bind_param($stmt, "s", $_SESSION['StudentID']);
                     mysqli_stmt_execute($stmt);
                     $result = mysqli_stmt_get_result($stmt);
+
                     // Storing records/rows into array
                     while($row = mysqli_fetch_array($result)){
                     ?>
 
                         <tr>
-                            <th class="h_th"><a href="sreply_interface.php?ticketnum=<?php echo $row['ticket_number'];?>"><?php echo $row['ticket_number'];?></a></th>
-                            <th class="h_th"><?php echo $row['FacultyID'];?></th>
-                            <th class="h_th"><?php echo $row['Subject'];?></th>
-                            <th class="h_th"><?php echo $row['Status'];?></th>
-                            <th class="h_th"><?php echo date("m/d/Y", strtotime($row["DateCreated"]));?></th>
+                            <th><a href="sreply_interface.php?ticketnum=<?php echo $row['ticket_number'];?>"><?php echo $row['ticket_number'];?></a></th>
+                            <th><?php echo $row['FacultyName'];?></th>
+                            <th><?php echo $row['Subject'];?></th>
+                            <th><?php echo $row['Status'];?></th>
+                            <th><?php echo date("m/d/Y", strtotime($row["DateCreated"]));?></th>
                         </tr>
 
                     <?php
