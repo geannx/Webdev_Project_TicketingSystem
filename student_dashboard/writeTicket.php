37abd6ffc7c96b8f12e19d0930ea68a47cc68434
geannx <santiago.geanne@gmail.com>
2022-02-28 01:28:05 +0800

    PARA SA STUDENT DASHBOARD!!!
    final final finalll!!!

--- student_dashboard/writeTicket.php
+++ student_dashboard/writeTicket.php
@@ -20,12 +20,12 @@
         <form action="writeTicket_input.php" method="POST">
             <label id="send_to">Send to:</label>
             <div class="select">
-                <select name="FacultyID" id="faculty">
-                <option selected disabled>----------</option>
+                <select name="FacultyID" id="faculty" required>
+                <option value="">----------</option>
                 <?php
                 //Loop to display all faculty head student is enrolled in
-                    $sql = "SELECT FacultyName, class_head FROM faculty f JOIN class_list cl ON f.FacultyID = cl.class_head WHERE FacultyID IN 
-                            (SELECT class_head FROM class_list cl JOIN class_details cd ON cl.class_id = cd.class_id WHERE StudentID = ?);" ;
+                $sql = "SELECT FacultyName, class_head, class_id FROM faculty f JOIN class_list cl ON f.FacultyID = cl.class_head WHERE FacultyID IN
+                        (SELECT class_head FROM class_list cl JOIN class_details cd ON cl.class_id = cd.class_id WHERE StudentID = ?);";
                     $stmt = mysqli_stmt_init($conn);
                     mysqli_stmt_prepare($stmt, $sql);
                     mysqli_stmt_bind_param($stmt, "s", $_SESSION['StudentID'] );
@@ -33,17 +33,17 @@
                     $result = mysqli_stmt_get_result($stmt);
                     while($row = mysqli_fetch_assoc($result)){
                 ?> 
-                    <option value="<?php echo $row['class_head']; ?>"><?php echo $row['FacultyName']; ?></option>
+                    <option value="<?php echo $row['class_head']; ?>"><?php echo $row['FacultyName'];?>&emsp;[<?php echo $row['class_id'];?>]</option>
                     <?php } ?>
                 </select>
             </div><br><br>
 
             <label id = "Lsubject">Subject: </label>
-            <input type="text" id="t_subject" name ="Subject" placeholder="Enter subject"> 
+            <input type="text" id="t_subject" name ="Subject" placeholder="Enter subject" required> 
             <div class="text_input">
 
                 <!-- TEXT AREA -->
-                <textarea id="message" name="MsgBody"></textarea>
+                <textarea id="message" name="MsgBody" required></textarea>
 
                 <!-- NOTE AREA -->
                 <p id="note" >Note: After clicking the submit button, please refer to "View History" for the ticket number that you would need for future references.</p>
