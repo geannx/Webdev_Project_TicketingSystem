83120de679db44a10292900e6004679063eb69ac
Mark Steven Reyes <mstevenreyes@gmail.com>
2022-02-28 07:46:45 +0800

    Update freply_interface_inc.php

--- faculty_dashboard/freply_interface_inc.php
+++ faculty_dashboard/freply_interface_inc.php
@@ -9,3 +9,4 @@
     mysqli_stmt_bind_param($stmt, "sss", $_SESSION['ticketnum'], $_SESSION['FacultyID'], $Body);
     mysqli_stmt_execute($stmt);
     // mysqli_query($conn, $sql);
+    header ('location: freply_interface.php?ticketnum=' . $_SESSION['ticketnum']);
